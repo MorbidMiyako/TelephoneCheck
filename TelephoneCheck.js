e46807a2a247afcd6e0c121a3e29c19d13970699
MorbidMiyako <57117884+MorbidMiyako@users.noreply.github.com>
2019-11-29 21:43:13 +0100

    threw the initial design in

---
+++ TelephoneCheck.js
@@ -0,0 +1,12 @@
+function TelephoneCheck(str) {
+    //list of variables
+    let numbers = [1,2,3,4,5,6,7,8,9]
+
+    if (str.Match(/-/g) === true) {
+        console.log("hey")
+    }
+}
+
+TelephoneCheck('1232-31253156')
+
+
